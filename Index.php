0951620fe72ed7382ce83ee626f88b71cb69acf6
LukicA-BPC-Cyan <32195127+LukicA-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:36:11 +0000

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$t = (rand(1,6));
+echo ($t)
+
+if $t = 1 {
+	echo "Almost Snake Eyes!";
+	}
+	elseif $t = 6 {
+	}
+	echo "Winner!";
+	}
+	else {
+	}
+?>
+
+</body>
+</html>
